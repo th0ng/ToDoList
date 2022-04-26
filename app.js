698f57b2d72ad70c5c5e32dba3756df4ea8507e9
Thong Hoang <97589967+th0ng@users.noreply.github.com>
2022-04-26 14:58:18 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+//jshint esversion:6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const date = require(__dirname + "/date.js");
+
+
+let items = [];
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    let day = date.getDate();
+    res.render('list', {listTitle: day, newListItems: items});
+});
+
+app.post('/', (req, res) => {
+    const item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+});
+
+app.get("/about", (req, res) => {
+    res.render('about');
+})
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+})
